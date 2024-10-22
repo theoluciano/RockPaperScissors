9273ac353ae2deb63aeb30f3d6be85b7d272922e
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-22 16:16:21 -0400

    Initial Commit

---
+++ RockPaperScissorsFight/RockPaperScissorsFightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsFightApp.swift
+//  RockPaperScissorsFight
+//
+//  Created by Theo Luciano on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsFightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorsFightTests/RockPaperScissorsFightTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsFightTests.swift
+//  RockPaperScissorsFightTests
+//
+//  Created by Theo Luciano on 10/22/24.
+//
+
+import Testing
+@testable import RockPaperScissorsFight
+
+struct RockPaperScissorsFightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
